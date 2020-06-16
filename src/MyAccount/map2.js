174241fb8307d94e85f6b4e9e3fb3a2459bee589
grusnny <37426267+grusnny@users.noreply.github.com>
2020-06-15 23:06:30 -0500

    brokker funcional

--- src/MyAccount/map2.js
+++ src/MyAccount/map2.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import { Map as Map,TileLayer, Marker, Popup } from 'react-leaflet';
 import "../MyAccount/map2.css";
+import * as L from 'leaflet'
+import icon from '../Home/marker2.webp';
+var subget=JSON.parse('{"lat": 6.26739785475676,"lng":-75.56881427764894}');;
+var greenIcon = L.icon({
+    iconUrl: icon,
+    //shadowUrl: shadow,
+
+    iconSize: [80, 80], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [40, 40], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 const styles = {
     wrapper: { 
       height: '100%', 
@@ -20,19 +34,60 @@ class MapExample extends Component {
         };
         this.handleClick = this.handleClick.bind(this);
     }
-handleClick(e){
-this.setState({ currentPos: e.latlng });
-const { lat, lng } = e.latlng;
-console.log(lat, lng);
+    handleClick(e){
+        var mqtt    = require('mqtt');
+        var client  = mqtt.connect('wss://mqtt.flespi.io',{
+        will: {
+            topic: 'test',
+            qos: 1,
+            retain: true,
+            properties: {
+            willDelayInterval: 120 /* MQTT 5.0 property saying how many seconds to wait before publishing the LWT message */
+            }
+        },
+        username: 'FlespiToken fmZr12yVuqMjLEBr3n2FLPINBPzRBmpGweVn4k7oAHybbXr38hyKW4nlKK3bXgFj'
+        });
+        console.log('mqtt client created, connecting...');
 
-window.localStorage.setItem("userLatDoc", lat);
-window.localStorage.setItem("userLngDoc", lng);
-}
+        client.on('connect', () => {
+        console.log('connected, subscribing to "Dispositivo1/GPS" topic...');
+
+        client.subscribe('Dispositivo1/GPS', {qos: 1}, (err) => {
+            if (err) {
+            console.log('failed to subscribe to topic "Dispositivo1/GPS":', err);
+            return;
+            }
+        });
+        });
+
+        client.on('message', (topic, msg) => {
+        subget=JSON.parse(msg.toString('utf8'));
+        console.log(`received message in topic "${topic}": "${msg.toString('utf8')}"`);
+        console.log('disconnecting...');
+        client.end();
+        });
+        
+        client.on('message', (topic, msg) => {
+        subget=JSON.parse(msg.toString('utf8'));
+        console.log(`received message in topic "${topic}": "${msg.toString('utf8')}"`);
+        console.log('disconnecting...');
+        client.end();
+    });
+        client.on('close', () => {
+        console.log('disconnected');
+        })
+        
+        client.on('error', (err) => {
+        console.log('mqtt client error:', err);
+        client.end(true) // force disconnect and stop the script
+        });
+
+    }
     render() {
         return (
         <div style={styles.wrapper}>
                 <Map    style={styles.map}
-                        center={[6.26739785475676,-75.56881427764894]}
+                        center={[subget.lat,subget.lng]}
                         zoom={16}
                         maxZoom={20}
                         attributionControl={true}
@@ -46,11 +101,12 @@ window.localStorage.setItem("userLngDoc", lng);
                     <TileLayer
                     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                     />
-                    { this.state.currentPos && <Marker position={this.state.currentPos} draggable={true}>
-                    <Popup position={this.state.currentPos}>
-                        Current location: <pre>{JSON.stringify(this.state.currentPos, null, 2)}</pre>
-                    </Popup>
-                    </Marker>}
+                    <Marker
+                            draggable='true'
+                            icon={greenIcon}
+                            position={[subget.lat, subget.lng]}
+
+                        />
                 </Map>
         </div>
         )
